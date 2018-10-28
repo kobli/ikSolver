5a7edb26f291dec54d265894b19fd8ee6215680f
Ales Koblizek <koblizek.ales@seznam.cz>
2018-10-28 14:32:05 +0100

    FABRIK without constraints, working in 2D

--- solver/include/chain.hpp
+++ solver/include/chain.hpp
@@ -7,6 +7,9 @@ namespace ik {
 	template<typename V>
 	class Chain {
 		public:
+			Chain(): _baseJointID{0}
+			{}
+
 			// the joints should be added starting from the base
 			// expects absolute positions of the joints
 			void appendJoint(const Joint<V>& j) {
@@ -15,6 +18,18 @@ namespace ik {
 
 			Joint<V>& getJoint(unsigned jointID) {
 				return _joints[jointID];
+			}
+
+			const Joint<V>& getJoint(unsigned jointID) const {
+				return _joints[jointID];
+			}
+
+			unsigned jointCount() const {
+				return _joints.size();
+			}
+
+			unsigned baseJointID() const {
+				return _baseJointID;
 			}
 
 		private:
--- solver/include/solver.hpp
+++ solver/include/solver.hpp
@@ -1,13 +1,47 @@
 #ifndef SOLVER_HPP_18_10_28_09_56_06
 #define SOLVER_HPP_18_10_28_09_56_06 
+#include <cassert>
+#include <iostream>
+#include <limits>
 #include "chain.hpp"
 
 namespace ik {
 	namespace FABRIK {
 		template<typename V>
-			void solveChain(Chain<V>& chain, unsigned jointID, V newPos)
+			void solveChainBidirectional(Chain<V>& chain, unsigned endEffectorID, unsigned baseID, V newPos)
 			{
-				chain.getJoint(jointID).position = newPos;
+				V prevJointPrevPos = chain.getJoint(endEffectorID).position;
+				V prevJointCurPos = newPos;
+				assert(endEffectorID != baseID);
+
+				int inc = endEffectorID > baseID? -1: 1;
+				for(int i = endEffectorID; i != int(baseID+inc); i += inc) {
+					Joint<V>& j = chain.getJoint(i);
+					float boneLength = (j.position-prevJointPrevPos).getLength();
+					newPos = prevJointCurPos + (j.position-prevJointCurPos).normalize()*boneLength;
+					prevJointCurPos = newPos;
+					prevJointPrevPos = j.position;
+					j.position = newPos;
+				}
+			}
+
+		template<typename V>
+			void solveChain(Chain<V>& chain, unsigned jointID, V newPos, float epsilon = 0.1, unsigned maxIter = 10)
+			{
+				unsigned baseID = chain.baseJointID();
+				V initialBasePosition = chain.getJoint(baseID).position;
+				unsigned i = 0;
+				float lastEpsilon = std::numeric_limits<float>::max();
+				do {
+					solveChainBidirectional(chain, jointID, baseID, newPos);
+					solveChainBidirectional(chain, baseID, jointID, initialBasePosition);
+					float newEpsilon = (newPos-chain.getJoint(jointID).position).getLength();
+					if(newEpsilon >= lastEpsilon) {
+						std::cerr << "FABRIK is diverging.\n";
+					}
+					lastEpsilon = newEpsilon;
+					++i;
+				}while(lastEpsilon > epsilon && i < maxIter);
 			}
 	}
 }
