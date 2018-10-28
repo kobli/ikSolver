d186b51e248e0e075227b715691e695d98739faf
Ales Koblizek <koblizek.ales@seznam.cz>
2018-10-28 12:39:22 +0100

    finish visualizer

--- visualizer/src/main.cpp
+++ visualizer/src/main.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <irrlicht.h>
 #include "solver.hpp"
 using namespace std;
@@ -17,31 +16,14 @@ using Joint = ik::Joint<Vector>;
 class MyEventReceiver : public IEventReceiver
 {
 	public:
-		// This is the one method that we have to implement
 		virtual bool OnEvent(const SEvent& event)
 		{
-			// Remember whether each key is down or up
-			if (event.EventType == irr::EET_KEY_INPUT_EVENT)
-				KeyIsDown[event.KeyInput.Key] = event.KeyInput.PressedDown;
-
+			if(event.EventType == irr::EET_MOUSE_INPUT_EVENT)
+				LMBpressed = event.MouseInput.isLeftPressed();
 			return false;
 		}
-
-		// This is used to check whether a key is being held down
-		virtual bool IsKeyDown(EKEY_CODE keyCode) const
-		{
-			return KeyIsDown[keyCode];
-		}
-
-		MyEventReceiver()
-		{
-			for (u32 i=0; i<KEY_KEY_CODES_COUNT; ++i)
-				KeyIsDown[i] = false;
-		}
-
-	private:
-		// We use this array to store the current state of each key
-		bool KeyIsDown[KEY_KEY_CODES_COUNT];
+		
+		bool LMBpressed = false;
 };
 
 void drawChain(IVideoDriver* driver, const Chain& chain) {
@@ -58,15 +40,12 @@ void drawChain(IVideoDriver* driver, const Chain& chain) {
 int main()
 {
 	MyEventReceiver receiver;
-	IrrlichtDevice *device =
-		createDevice( video::EDT_OPENGL, dimension2d<u32>(1024, 768), 16, false, false, false, &receiver);
+	IrrlichtDevice *device = createDevice( video::EDT_OPENGL, dimension2d<u32>(1024, 768), 16, false, false, false, &receiver);
 	if (!device)
 		return 1;
 	device->setWindowCaption(L"IK visualizer");
 	device->setResizable(true);
 	IVideoDriver* driver = device->getVideoDriver();
-	ISceneManager* smgr = device->getSceneManager();
-	IGUIEnvironment* guienv = device->getGUIEnvironment();
 
 	Chain chain;
 	chain.appendJoint({Vector(230)});
@@ -77,12 +56,9 @@ int main()
 	while(device->run())
 	{
 		driver->beginScene(true, true, SColor(255,100,101,140));
-
+		if(receiver.LMBpressed)
+			ik::FABRIK::solveChain(chain, chain.jointCount()-1, device->getCursorControl()->getPosition());
 		drawChain(driver, chain);
-
-		smgr->drawAll();
-		guienv->drawAll();
-
 		driver->endScene();
 	}
 	device->drop();
